e34258c5c05bfeddfb25c4090c4a5f34376f597c
Davis Kirkendall <davis.kirkendall@rwth-aachen.de>
2016-12-30 14:34:24 +0100

    Add Settings class to top level package namespace

--- climatecontrol/__init__.py
+++ climatecontrol/__init__.py
@@ -1,1 +1,12 @@
+#!/usr/bin/env python
 
+"""
+CLIMATECONTROL controls your apps configuration environment. It is a Python
+library for loading app configurations from files and/or namespaced environment
+variables.
+
+:licence: MIT, see LICENSE file for more details.
+
+"""
+
+from settings_parser import Settings  # noqa: F401
