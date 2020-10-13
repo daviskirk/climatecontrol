b8cdfca42222c82a293c1967754316a6b651fbda
Davis Kirkendall <davis.e.kirkendall@gmail.com>
2020-10-13 11:16:50 +0200

    Add python 3.9 to supported versions

--- setup.py
+++ setup.py
@@ -48,6 +48,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     setup_requires=["pytest-runner", "setuptools_scm"],
     extras_require={
